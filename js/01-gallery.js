5813447083257e93d2403123b02fad22a047ee17
Oleg Savchuk <brandoleg@gmail.com>
2021-11-11 20:11:58 +0200

    fisrt event finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galeryBox = document.querySelector('.gallery');
+
+const geleryMarkup = galleryItems.map((item) => {
+   return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>
+  `
+
+}).join('');
+
+galeryBox.innerHTML = geleryMarkup;
+
+function bigImage(e) {
+    e.preventDefault()
+
+    if(e.target === e.currentTarget) {
+        return
+    }
+
+    const originalImg = e.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${originalImg}" width="800" height="600">
+    
+`)
+
+instance.show()
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+    instance.close()
+        }
+    })
+
+};
+
+galeryBox.addEventListener('click', bigImage);
+
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,10 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const instance = galleryItems.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()
